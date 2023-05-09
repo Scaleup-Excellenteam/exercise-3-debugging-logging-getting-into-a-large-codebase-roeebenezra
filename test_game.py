f5940135441196d1a93e2dea19f098c4f67fc9fb
Roee <roibenezra0@gmail.com>
2023-05-09 15:03:53 +0300

    test_valid_piece_movestest_evaluate_board_black_player

--- test_game.py
+++ test_game.py
@@ -1,79 +1,78 @@
+import unittest
+
 from chess_engine import game_state
 from enums import Player
 from Piece import Knight
+from ai_engine import chess_ai
 
 
-def test_knight_valid_peaceful_moves():
-    # Set up the game state
-    gs = game_state()
+class TestChessEngine(unittest.TestCase):
+    def setUp(self):
+        self.gs = game_state()
 
-    # Set up the knight
-    white_knight = Knight('n', 3, 4, Player.PLAYER_2)
+    # Unit tests
+    def test_knight_valid_peaceful_moves(self):
+        # Set up the knight
+        white_knight = Knight('n', 3, 4, Player.PLAYER_2)
 
-    # Set up Empty board
-    gs.board = [[Player.EMPTY for _ in range(8)] for _ in range(8)]
+        # Set up Empty board
+        self.gs.board = [[Player.EMPTY for _ in range(8)] for _ in range(8)]
 
-    # Set Knight on the board
-    gs.board[3][4] = white_knight
+        # Set Knight on the board
+        self.gs.board[3][4] = white_knight
 
-    # Get the moves for the knight
-    moves = white_knight.get_valid_peaceful_moves(gs)
+        # Get the moves for the knight
+        moves = white_knight.get_valid_peaceful_moves(self.gs)
 
-    # # Assert that the expected moves are in the list
-    expected_moves = [(1, 3), (1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
+        # # Assert that the expected moves are in the list
+        expected_moves = [(1, 3), (1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
 
-    assert set(moves) == set(expected_moves)
+        assert set(moves) == set(expected_moves)
 
+    def test_knight_valid_peaceful_moves_blocked(self):
+        # Set up the knight
+        white_knight = Knight('n', 3, 4, Player.PLAYER_2)
 
-def test_knight_valid_peaceful_moves_blocked():
-    # Set up the game state
-    gs = game_state()
+        # Set Knight on the board
+        self.gs.board[3][4] = white_knight
 
-    # Set up the knight
-    white_knight = Knight('n', 3, 4, Player.PLAYER_2)
+        # Get the moves for the knight
+        moves = white_knight.get_valid_peaceful_moves(self.gs)
 
-    # Set Knight on the board
-    gs.board[3][4] = white_knight
+        # # Assert that the expected moves are in the list
+        expected_moves = [(2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
 
-    # Get the moves for the knight
-    moves = white_knight.get_valid_peaceful_moves(gs)
+        assert set(moves) == set(expected_moves)
 
-    # # Assert that the expected moves are in the list
-    expected_moves = [(2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
+    def test_knight_valid_piece_takes(self):
+        # Set up the knight
+        white_knight = Knight('n', 3, 4, Player.PLAYER_2)
 
-    assert set(moves) == set(expected_moves)
+        # Get the takes moves for the knight
+        moves = white_knight.get_valid_piece_takes(self.gs)
 
+        # # Assert that the expected moves are in the list
+        expected_moves = [(1, 3), (1, 5)]
 
-def test_knight_valid_piece_takes():
-    # Set up the game state
-    gs = game_state()
+        assert set(moves) == set(expected_moves)
 
-    # Set up the knight
-    white_knight = Knight('n', 3, 4, Player.PLAYER_2)
+    # Integration tests
+    def test_valid_piece_moves(self):
+        # Set up the knight
+        white_knight = Knight('n', 3, 4, Player.PLAYER_2)
 
-    # Get the takes moves for the knight
-    moves = white_knight.get_valid_piece_takes(gs)
+        # Set Knight on the board
+        self.gs.board[3][4] = white_knight
 
-    # # Assert that the expected moves are in the list
-    expected_moves = [(1, 3), (1, 5)]
+        # Get the moves for the knight
+        moves = white_knight.get_valid_piece_moves(self.gs)
 
-    assert set(moves) == set(expected_moves)
+        # # Assert that the expected moves are in the list
+        expected_moves = [(1, 3), (1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
 
+        assert set(moves) == set(expected_moves)
 
-def test_valid_piece_moves():
-    # Set up the game state
-    gs = game_state()
-
-    # Set up the knight
-    white_knight = Knight('n', 3, 4, Player.PLAYER_2)
-
-    # Set Knight on the board
-    gs.board[3][4] = white_knight
-
-    # Get the moves for the knight
-    moves = white_knight.get_valid_piece_moves(gs)
-
-    # # Assert that the expected moves are in the list
-    expected_moves = [(1, 3), (1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
-
-    assert set(moves) == set(expected_moves)
+    def test_evaluate_board_black_player(self):
+        self.gs.board[6][0] = Player.EMPTY  # Remove opponent pawn, +10 points
+        evaluate_board = chess_ai().evaluate_board(self.gs, Player.PLAYER_2)
+        assert evaluate_board == 10
