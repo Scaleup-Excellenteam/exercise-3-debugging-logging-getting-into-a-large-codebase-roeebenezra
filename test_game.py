85d3a4111b90b8d9875665669d8b51a388fa1e22
Roee <roibenezra0@gmail.com>
2023-05-09 12:41:11 +0300

    test_knight_valid_peaceful_moves_blocked

--- test_game.py
+++ test_game.py
@@ -3,15 +3,17 @@ from enums import Player
 from Piece import Knight
 
 
-def test_knight_get_valid_peaceful_moves():
+def test_knight_valid_peaceful_moves():
     # Set up the game state
     gs = game_state()
 
     # Set up the knight
     white_knight = Knight('n', 3, 4, Player.PLAYER_2)
 
-    # Set Knight on the board
+    # Set up Empty board
     gs.board = [[Player.EMPTY for _ in range(8)] for _ in range(8)]
+
+    # Set Knight on the board
     gs.board[3][4] = white_knight
 
     # Get the moves for the knight
@@ -19,5 +21,24 @@ def test_knight_get_valid_peaceful_moves():
 
     # # Assert that the expected moves are in the list
     expected_moves = [(1, 3), (1, 5), (2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
+
+    assert set(moves) == set(expected_moves)
+
+
+def test_knight_valid_peaceful_moves_blocked():
+    # Set up the game state
+    gs = game_state()
+
+    # Set up the knight
+    white_knight = Knight('n', 3, 4, Player.PLAYER_2)
+
+    # Set Knight on the board
+    gs.board[3][4] = white_knight
+
+    # Get the moves for the knight
+    moves = white_knight.get_valid_peaceful_moves(gs)
+
+    # # Assert that the expected moves are in the list
+    expected_moves = [(2, 2), (2, 6), (4, 2), (4, 6), (5, 3), (5, 5)]
 
     assert set(moves) == set(expected_moves)
